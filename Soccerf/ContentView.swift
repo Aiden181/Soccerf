08c8342dd15f5145977c603df681f80405ab4872
khoiviet181 <49256279+khoiviet181@users.noreply.github.com>
2022-08-01 20:43:31 +0700

    Initial Commit

---
+++ Soccerf/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Soccerf
+//
+//  Created by Aiden Ly on 01/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Soccerf/SoccerfApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoccerfApp.swift
+//  Soccerf
+//
+//  Created by Aiden Ly on 01/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SoccerfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
